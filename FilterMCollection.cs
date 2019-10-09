1e8e8c13f0f2ace2e758795c4712d79c6223ddf0
Silviu Bogan <silviubogan@gmail.com>
2019-10-09 15:08:08 +0300

    Added code files

---
+++ FilterMCollection.cs
@@ -0,0 +1,153 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_timed_silver
+{
+    public class FilterMCollection : BindableBase
+    {
+        public ObservableCollection<FilterM> Ms;
+
+        internal ClockMCollection MyClocks;
+
+        public FilterMCollection(ClockMCollection c)
+        {
+            MyClocks = c;
+
+            Ms = new ObservableCollection<FilterM>();
+            Ms.CollectionChanged += Ms_CollectionChanged;
+        }
+
+        private OcPropertyChangedListener<FilterM> l1;
+        private OcPropertyChangedListener<ClockGroupM> l2;
+
+        public void Initialize()
+        {
+            l1 = OcPropertyChangedListener.Create(MyClocks.Filters.Ms);
+            l1.PropertyChanged += l1_PropertyChanged;
+
+            l2 = OcPropertyChangedListener.Create(MyClocks.Groups.Ms);
+            l2.PropertyChanged += l2_PropertyChanged;
+        }
+
+        private void l2_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            var g = sender as ClockGroupM;
+
+            foreach (FilterM f in MyClocks.Filters.Ms)
+            {
+                if (!f.IsBaseFilter && g.Name == f.GroupNames.FirstOrDefault())
+                {
+                    f.DisplayString = g.Name;
+                    f.IsSelected = g.IsSelected;
+                    f.IsBaseFilter = false;
+
+                    f.MyConstantImageSource = g.Icon;
+                    f.MyEmptyImageSource = MyClocks.FiltersVM.MyEmptyFolderIcon;
+                    f.MyNonEmptyImageSource = MyClocks.FiltersVM.MyNonEmptyFolderIcon;
+
+                    f.GroupNames.Clear();
+                    f.GroupNames.Add(g.Name);
+
+                    f.UpdateItemCount();
+                }
+                else
+                {
+                    if (!f.GroupNames.Contains(gv))
+                    {
+                        continue;
+                    }
+
+                    var ng = new ObservableCollection<string>(f.GroupNames);
+
+                    ng.Remove(gv);
+                    ng.Add(g.Name);
+
+                    f.GroupNames = ng;
+                    f.DisplayString = g.Name;
+                }
+            }
+        }
+
+        private void l1_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            var f = sender as FilterM;
+
+            if (f.GroupNames.Count == 0 || f.GroupNames.Count > 1)
+            {
+                return;
+            }
+
+            foreach (ClockGroupM g in MyClocks.Groups.Ms)
+            {
+                if (g.Name == f.GroupNames.FirstOrDefault())
+                {
+                    g.Name = f.DisplayString;
+                    g.Icon = f.MyConstantImageSource;
+                    g.IsSelected = f.IsSelected;
+                    break;
+                }
+            }
+        }
+
+        private void Ms_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+
+            if (MyClocks.GroupsVM.SynchDisabled2)
+            {
+
+            }
+            else
+            {
+                MyClocks.GroupsVM.SynchDisabled2 = true;
+
+                Utils.SynchronizeCollectionChange(Ms, e, MyClocks.Groups.Ms,
+                    afterAddition: null,
+                    prepareDeletion: null,
+                    equalsWithinTargetTo: (FilterM f, ClockGroupM g) =>
+                    {
+                        if (f.GroupNames.Count == 0)
+                        {
+                            return false;
+                        }
+                        return g.Name == f.DisplayString;
+                    },
+                    toTarget: (FilterM m) =>
+                    {
+                        if (m.GroupNames.Count == 0)
+                        {
+                            return null;
+                        }
+
+                        foreach (ClockGroupM g in MyClocks.Groups.Ms)
+                        {
+                            if (m.GroupNames.Count < 1)
+                            {
+                                continue;
+                            }
+                            if (m.GroupNames[0] == g.Name)
+                            {
+                                return g;
+                            }
+                        }
+
+                        return new ClockGroupM()
+                        {
+                            Name = m.DisplayString,
+                            Icon = m.MyImageSource,
+                            IsSelected = m.IsSelected
+                        };
+                    },
+                    startingIndexInSource: 5,
+                    startingIndexInTarget: 0);
+
+                MyClocks.GroupsVM.SynchDisabled2 = false;
+            }
+        }
+    }
+}
